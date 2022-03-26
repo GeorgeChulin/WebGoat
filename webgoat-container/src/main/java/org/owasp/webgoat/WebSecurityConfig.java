e99c21f44eacba956e72c4e2e76d25c58146c47f
GeorgeChulin <j.garciaga.2019@alumnos.urjc.es>
2022-03-26 01:12:08 +0100

    Update bcrypt

--- webgoat-container/src/main/java/org/owasp/webgoat/WebSecurityConfig.java
+++ webgoat-container/src/main/java/org/owasp/webgoat/WebSecurityConfig.java
@@ -43,7 +43,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.bCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 /**
  * Security configuration for WebGoat.
@@ -78,7 +79,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.passwordEncoder(new bCryptPasswordEncoder()); //.passwordEncoder(bCryptPasswordEncoder());
+        auth.userDetailsService(null).passwordEncoder(new BCryptPasswordEncoder()); //.passwordEncoder(bCryptPasswordEncoder());
     }
 
     @Bean
